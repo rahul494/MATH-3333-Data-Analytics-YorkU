a82540b971308b111613fdc6fe6883a9144feb56
Rahul Sharma <rahul494@my.yorku.ca>
2020-02-12 19:51:05 -0500

    Create A2Q5.R

---
+++ Assignment 2/A2Q5.R
@@ -0,0 +1,12 @@
+################################################################################################
+# Name: Rahul Sharma
+# ID: 214393508
+# Assignment: 2
+# Question: 5
+################################################################################################
+
+# Perform the logistic regression on the dataset. You can some of the predictors in the
+# model. Please use 900 observations as the training set and use your model to predict the
+# default status of the remaining 100 loans.
+
+credit <- read.csv("C:/Users/Rahul/Downloads/credit.csv")
