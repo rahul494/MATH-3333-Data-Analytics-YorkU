a262f1d42305fc54c86d6fd2536c3ef6c86120a2
Rahul Sharma <rahul494@my.yorku.ca>
2020-02-16 18:20:38 -0500

    Update A2Q5.R

--- Assignment 2/A2Q5.R
+++ Assignment 2/A2Q5.R
@@ -5,8 +5,17 @@
 # Question: 5
 ################################################################################################
 
-# Perform the logistic regression on the dataset. You can some of the predictors in the
+# a) Perform the logistic regression on the dataset. You can some of the predictors in the
 # model. Please use 900 observations as the training set and use your model to predict the
 # default status of the remaining 100 loans.
 
-credit <- read.csv("C:/Users/Rahul/Downloads/credit.csv")
+credit <- read.csv("C:/Users/Rahul/Downloads/german_credit_dataset.csv")
+
+sample_size = round(nrow(credit)*.90)
+index <- sample(seq_len(nrow(credit)), size = sample_size)
+
+credit.train <- credit[index, ]
+credit.test <- credit[-index, ]
+
+# b) Please investigate how the false negative number and false positive number change
+# with respect to the different cutoff value of probability.
