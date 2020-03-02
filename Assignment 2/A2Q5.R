43981a8d98efd4280b407c85ae99d999a24e3325
Rahul Sharma <rahul494@my.yorku.ca>
2020-03-02 15:55:10 -0500

    Update A2Q5.R

--- Assignment 2/A2Q5.R
+++ Assignment 2/A2Q5.R
@@ -9,13 +9,32 @@
 # model. Please use 900 observations as the training set and use your model to predict the
 # default status of the remaining 100 loans.
 
-credit <- read.csv("C:/Users/Rahul/Downloads/german_credit_dataset.csv")
+credit <- read.csv("C:/Users/Rahul/Downloads/credit.csv")
 
+# use 90% of the dataset to be used for training
 sample_size = round(nrow(credit)*.90)
 index <- sample(seq_len(nrow(credit)), size = sample_size)
 
 credit.train <- credit[index, ]
 credit.test <- credit[-index, ]
+ynew <- credit$credit.rating[-index]
+
+cred.glm = glm(credit.rating ~., family=binomial, data=credit.train)
+summary(cred.glm)
+
+ptest <- predict(cred.glm, newdata = credit.test, type="response")
+data.frame(ynew, ptest)
+gg1 = floor(ptest + (5/6))
+ttt = table(ynew, gg1)
+ttt
+error = (ttt[1,2] + ttt[2,1])/100
+error
 
 # b) Please investigate how the false negative number and false positive number change
 # with respect to the different cutoff value of probability.
+
+gg1 = floor(ptest + (1/2))
+ttt = table(ynew, gg1)
+ttt
+error = (ttt[1,2] + ttt[2,1])/100
+error
